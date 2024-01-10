b3ca254dcd6a93e016f4bee2565dc176a9a1b6ea
tomspyropoulos <tomody.spy@gmail.com>
2024-01-10 15:28:19 +0000

    Added script for running all queries

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+start-all.sh
+hdfs dfsadmin -safemode leave
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+venv-pack -o venv.tar.gz
+export PYSPARK_PYTHON=./venv/bin/python
+spark-submit --archives venv.tar.gz#venv req_1.py
+spark-submit --archives venv.tar.gz#venv query_1.py
+spark-submit --archives venv.tar.gz#venv query_1_sql.py
+spark-submit --archives venv.tar.gz#venv query_2.py
+spark-submit --archives venv.tar.gz#venv query_2_rdd.py
+spark-submit --archives venv.tar.gz#venv query_3.py
+spark-submit --archives venv.tar.gz#venv query_4_a.py
+spark-submit --archives venv.tar.gz#venv query_4_b.py
+deactivate
+stop-all.sh
+rm -rf venv
+rm venv.tar.gz
